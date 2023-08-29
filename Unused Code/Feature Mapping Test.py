f8035b36be2231fff9978a737aef6b4e47006c73
Sophie <ppysf5@nottingham.ac.uk>
2023-08-29 18:51:06 +0100

    Update feature mapping code
    Condensed things a little

--- Unused Code/Feature Mapping Test.py
+++ Unused Code/Feature Mapping Test.py
@@ -66,12 +66,9 @@ initialStd = np.nanstd(intensityImages[0])
 imageStack = []
 
 for image in intensityImages:
-    print("looping")
+    #normalising intensity
     avg = np.nanmean(image)
     std = np.nanstd(image)
-
-    #image = (image-np.nanmin(image))/(np.nanmax(image)-np.nanmin(image)) #renormalises - after contrast adjust so shows
-    #image = image * 255
 
     newImage = initialAvg + ((image - avg) * (initialStd / std))
 
@@ -81,7 +78,6 @@ for image in intensityImages:
 
     image = np.array(image, dtype="uint8")
 
-        
     imageStack = imageStack + [image]
  
 intensityImages = imageStack
@@ -96,7 +92,7 @@ for i in range(1, len(differenceImages)):
     
     # Initiate SIFT detector
     sift = cv2.SIFT_create()
-    #surf = cv2.SURF_cre
+    #surf = cv2.SURF_create()
     
     # find the keypoints and descriptors with SIFT
     # Here img1 and img2 are grayscale images
@@ -114,15 +110,13 @@ for i in range(1, len(differenceImages)):
     #                                 minDistance=30,   
     #                                 blockSize=3)
 
-    print("no of keypoints")
-    print(len(des1))
-    print(len(des2))
+
 
     # FLANN parameters
-    # I literally copy-pasted the defaults
     FLANN_INDEX_KDTREE = 1
     index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
     search_params = dict(checks=500)   # or pass empty dictionary
+
     # do the matching
     flann = cv2.FlannBasedMatcher(index_params,search_params)
     matches = flann.knnMatch(des1,des2,k=2)
@@ -138,60 +132,46 @@ for i in range(1, len(differenceImages)):
         # extract coordinated for query and train image points
         src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
         dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-        print("points")
-        print(src_pts)
+
         # use homography to get the M transformation matrix
-        #not working for rotations
         M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)  #returning 3d transformation matrix for some reason - crop out below to make work with rest of code
+        
+        #alternatively, find affine transform
         #M = cv2.estimateAffinePartial2D(src_pts, dst_pts)
         #M = M[:-1, :]
         
         matchesMask = mask.ravel().tolist()
-        # here im1 is the original RGB (or BGR because of cv2) image
+
         h,w = img1.shape
         pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
 
-        # use this transform to shift train image to query image
+        #another option to find transformation mstrix
         #dst = cv2.getPerspectiveTransform(pts,M)  
-        #print("dst")
-        #print(dst)
-        #print("shape")
-        #print(dst.shape)
-        #dst = np.array(dst, dtype = np.float32)
+
         img2 = np.array(img2, dtype = np.float32)
         im2 = cv2.warpPerspective(img2, M, (h, w))
+
         plt.imshow(im2, cmap="gray")
         plt.show()
 
 
     #correctedDifferenceImages = correctedDifferenceImages + [[np.array(dst, dtype="uint8")]]
-    # for now I only displayed it using polylines, depending on what you need you can use these points to do something else
-    # I overwrite the original RGB (or BGR) image with a red rectangle where the smaller image should be
+    # To display matches
     #im2 = cv2.polylines(img2,[np.int32(dst)],True,(0,0,255),10, cv2.LINE_AA)
 
-    #print("shape")
-    #print(img2.shape)
     #img2 = np.array(img2, dtype = np.float32)
     #im2 = cv2.warpAffine(img2, M, (w,h))
-    #print("shape")
-    #print(im2.shape)
-    #plt.imshow(im2, cmap="gray")
-    #plt.show()
-    #cv2.imwrite('image_overlap.png', im2) 
+    
     correctedDifferenceImages = correctedDifferenceImages + [im2]
 
 plt.figure()
 plt.axis("off")
 
 ax1 = plt.subplot(1, 2, 1)
-#print("weird shape")
-#print(correctedDifferenceImages.shape)
 
 img1 = correctedDifferenceImages[0]
 
-
 fig1 = ax1.imshow(img1, cmap="gray")
-
 
 #defines slider axis
 axs = plt.axes([0.15, 0.001, 0.65, 0.03])
