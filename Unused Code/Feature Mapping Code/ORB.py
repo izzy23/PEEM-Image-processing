fef161b9ae67ca940c9c6b4097e5b49d89c5b414
Sophie <ppysf5@nottingham.ac.uk>
2023-09-03 15:51:08 +0100

    More feature mapping code found

---
+++ Unused Code/Feature Mapping Code/ORB.py
@@ -0,0 +1,222 @@
+import numpy as np
+import h5py
+import cv2
+from skimage import io
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm  
+from matplotlib.widgets import Slider
+from skimage import io
+from skimage.io import imsave as sk_imsave
+
+def averageImages(imStack):
+
+    imageSum = imStack[0]
+
+    #adding pixel vals for each image
+    for image in range(1, len(imStack)):
+        
+        #can't remove float - 8bit wrap around weird thing
+        imageSum = imageSum.astype(float) + imStack[image].astype(float)
+        #imageSum = imageSum + imStack[image]
+
+    #becomes average
+    imageSum = imageSum / len(imStack)
+
+    return imageSum
+
+
+def rotateImg(img, da):
+
+    h, w = img.shape
+    center = (w / 2, h / 2)
+
+    scale = 1
+    m = cv2.getRotationMatrix2D(center, da, None)
+    rotatedImg = cv2.warpAffine(img, m, (w,h))
+    return rotatedImg
+
+def align_images(image1, image2):
+    # Initialize ORB detector
+    orb = cv2.ORB_create()
+
+    # Find keypoints and descriptors
+    kp1, des1 = orb.detectAndCompute(image1, None)
+    kp2, des2 = orb.detectAndCompute(image2, None)
+
+    # Initialize brute-force matcher
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    # Match descriptors
+    matches = bf.match(des1, des2)
+
+    # Sort matches by distance
+    matches = sorted(matches, key=lambda x: x.distance)
+
+    # Take the top N matches 
+    N = 50
+    matches = matches[:N]
+
+    # Extract matched keypoints
+    src_pts = np.float32([kp1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+
+    draw_params = dict(matchColor = (0,255,0),
+        singlePointColor = (255,0,0),
+        flags = cv2.DrawMatchesFlags_DEFAULT)
+
+    img3 = cv2.drawMatchesKnn(image1,kp1,image2,kp2,matches[:10],None,**draw_params)
+    plt.imshow(img3)
+    plt.show()
+
+    # Find the perspective transformation matrix
+    M, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+
+    # Apply the transformation to image1
+    aligned_image = cv2.warpPerspective(image2, M, (image1.shape[1], image2.shape[0]))
+
+    return aligned_image
+
+
+#startNo = 0
+#endNo = 5
+
+imageNumbers = ["321124-321125", "321127-321136", "321227-321236", "321457-321466", "321527-321536", "321617-321626", "321627-321636"]
+differenceImages = []
+intensityImages = []
+
+for i in imageNumbers: 
+
+    differenceImg = io.imread(r"RC294 - Cross Centre\%s_Diffference.tif" % str(i))
+    intensityImg = io.imread(r"RC294 - Cross Centre\%s_Intensity.tif" % str(i))
+
+    differenceImages = differenceImages  + [differenceImg]
+    intensityImages = intensityImages + [intensityImg]
+
+    
+
+initialIntensityImages = intensityImages
+initialDifferenceImages = differenceImages
+
+imageStack = []
+
+initialAvg = np.nanmean(differenceImages[0])
+initialStd = np.nanstd(differenceImages[0])
+
+for image in differenceImages:
+    avg = np.nanmean(image)
+    std = np.nanstd(image)
+
+    
+    newImage = initialAvg + ((image - avg) * (initialStd / std))
+    #print(newImage)
+    np.clip(newImage, -1, 1)
+    
+    image = newImage
+    image = (image + 1)/2
+    image = image * 255
+    image = np.array(image, dtype="uint8")
+    image = cv2.medianBlur(image, 5)
+    image = np.clip(image, 120, 140)
+    image = (image-np.nanmin(image))/(np.nanmax(image)-np.nanmin(image))
+    image = image * 255
+    image = np.array(image, dtype="uint8")
+
+    plt.imshow(image, cmap="gray")
+    plt.show()
+
+    imageStack = imageStack + [image]
+#imageStack = standardizeStack(imageStack)
+
+differenceImages = imageStack
+print("no of images")
+print(len(differenceImages))
+
+initialAvg = np.nanmean(intensityImages[0])
+initialStd = np.nanstd(intensityImages[0])
+imageStack = []
+
+
+for image in intensityImages:
+    print("looping")
+    avg = np.nanmean(image)
+    std = np.nanstd(image)
+
+    image = (image-np.nanmin(image))/(np.nanmax(image)-np.nanmin(image)) #renormalises - after contrast adjust so shows
+    image = image * 255
+
+    newImage = initialAvg + ((image - avg) * (initialStd / std))
+
+
+    np.clip(newImage, 0, 255)
+    image = np.array(newImage, dtype = "uint8")
+    image = cv2.medianBlur(image, 5)
+
+    image = np.array(image, dtype="uint8")
+
+        
+    imageStack = imageStack + [image]
+ 
+intensityImages = imageStack
+
+angles = [0, 0, -30, -45, -50, -50, -90]
+
+for i in range(1, len(intensityImages)):
+
+    intensityImages[i] = rotateImg(intensityImages[i], angles[i])
+    differenceImages[i] = rotateImg(differenceImages[i], angles[i])
+
+firstImg = differenceImages[0]
+for i in range(1, len(differenceImages)):
+    img = differenceImages[i]
+    differenceImages[i] = align_images(img, firstImg)
+
+correctedDifferences = differenceImages
+correctedIntensities = intensityImages
+
+plt.figure()
+plt.axis("off")
+
+ax1 = plt.subplot(2, 2, 1)
+ax2 = plt.subplot(2, 2, 2)
+ax3 = plt.subplot(2, 2, 3)
+ax4 = plt.subplot(2, 2, 4)
+
+img1 = correctedDifferences[0]
+img2 = correctedIntensities[0]
+
+fig1 = ax1.imshow(img1, cmap="gray")
+fig2 = ax2.imshow(img2, cmap="gray")
+
+
+#defines slider axis
+axs = plt.axes([0.15, 0.001, 0.65, 0.03])
+sb = Slider(axs, 'image no', 0, 6, valinit=0, valstep = 1)
+
+#runs when slider moved
+def update(val):
+    img1 = correctedDifferences[sb.val]
+    img2 = correctedIntensities[sb.val]
+
+    fig1.set_data(img1)
+    fig2.set_data(img2)
+        
+    plt.draw()
+
+ax1.set_title("differences")
+ax2.set_title("intensities")
+
+sb.on_changed(update)
+
+averageIntensity = averageImages(intensityImages)
+averageDifference = averageImages(differenceImages)
+
+ax4.imshow(averageIntensity, cmap="gray")
+ax3.imshow(averageDifference, cmap = "gray")
+
+#for i in range(0, len(correctedDifferences)):
+    #sk_imsave("rotatedCross/%s_D.tif" % (str(i)), correctedDifferences[i])
+    #sk_imsave("rotatedCross/%s_I.tif" % (str(i)), correctedIntensities[i])
+
+plt.show()
+
